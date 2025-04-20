53c88b40b1b9906bee183a8ee0376835915822ed
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2025-04-20 22:58:36 +0530

    Add files via upload

---
+++ service_key.py
@@ -0,0 +1,7 @@
+groq_key = "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"
+
+hugging_key = "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"
+
+google_key = "AIzaSyCh87N6ffCUi_v-XYD2mu_KYMMAEIoinTQ"
+
+anthropic_key = "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"
